6df0e5b2c5e892b73c9470d9a246d87dad99ec48
Dy ann <dyann.razonado.5@gmail.com>
2025-06-18 16:19:35 +0800

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+document.getElementById("increasebtn").onclick = function(){
+    count +=1;
+    document.getElementById("counter").innerHTML = count;
+}
+
+document.getElementById("decreasebtn").onclick = function(){
+    count -=1;
+    document.getElementById("counter").innerHTML = count;
+    
+}
+
+document.getElementById("resetbtn").onclick = function(){
+    count = 0;
+    document.getElementById("counter").innerHTML = count;
+}
---
+++ facebook.js
@@ -0,0 +1,42 @@
+const name = document.getElementById("name")
+const password = document.getElementById("password")
+const form = document.getElementById("form")
+const errorElement = document.getElementById("error")
+const cpassword = document.getElementById("cpassword")
+
+
+
+form.addEventListener("submit", (e) =>{
+
+    let messages = []
+    if(name.value === "" || name.value == null){
+        messages.push("Name required!")
+    }
+    
+    else if(password.value === "" || password.value == null){
+        messages.push("Password required")
+    }
+
+    else if(password.value === "password"){
+        messages.push("Password cannot be password!")
+
+    }
+
+    else if(password.value != cpassword){
+        messages.push("Password did'nt match")
+    }
+
+    if(messages.length > 0){
+        e.preventDefault
+        errorElement.innerText = messages.join(', ')
+    }
+
+    
+    
+
+
+
+
+
+
+})
